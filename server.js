6655a62281cbe54f59cb4900d8a08eeefa529a34
Mohamed Lembarki <lembarkimohamed4@gmail.com>
2022-02-23 23:43:10 +0100

    adding index.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
+app.use("/static", express.static(path.resolve(__dirname,"frontend","static")));
 
 app.get('/*',(req,res)=>{
     res.sendFile(path.resolve("frontend" , "index.html"));
